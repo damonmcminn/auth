26cc1ba7ba59370696f4aa79fbc919e7a99628c9
Damon McMinn <damonrmcminn@gmail.com>
2015-04-07 15:23:25 +0100

    Added #hash positive test cases

--- spec/passwordSpec.js
+++ spec/passwordSpec.js
@@ -10,13 +10,28 @@ describe('Password', function() {
         password.hash(1),
         password.hash({}),
         password.hash([])
-        ])
-        .then(function(values) {
-          values.forEach(function(val) {
-            expect(val).toBe(false);
-          });
+      ])
+      .then(function(values) {
+        values.forEach(function(val) {
+          expect(val).toBe(false);
+        });
+        done();
+      });
+    });
+
+    it('should return a hash', function(done) {
+      Promise.all([
+        password.hash('foo'),
+        password.hash('bar'),
+        password.hash(''),
+        password.hash('a very very long password in a phrase'),
+      ])
+      .then(function(values) {
+        values.forEach(function(val) {
+          expect(val.length).toBe(60);
           done();
         });
+      });
     });
 
   });
