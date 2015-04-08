bdfbe66a9850c061aff887d9b513f6e467f1e01d
Damon McMinn <damonrmcminn@gmail.com>
2015-04-08 14:42:30 +0100

    Add parseHeader to module exports

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ module.exports = Auth;
 
 const password = require('./lib/password');
 const token = require('./lib/token');
-
+const parseHeader = require('./lib/parseHeader');
 
 function Auth() {}
 
 Auth.password = password;
 Auth.token = token;
+Auth.parseHeader = parseHeader;
--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -2,9 +2,10 @@ var auth = require('../index');
 
 describe('auth', function() {
 
-  it('should return an object with props: #password, #token', function() {
+  it('should return an object with props: #password, #token, #parseHeader', function() {
     expect(auth.password).toEqual(jasmine.anything());
     expect(auth.token).toEqual(jasmine.anything());
+    expect(auth.parseHeader).toEqual(jasmine.anything());
   });
 
 });
